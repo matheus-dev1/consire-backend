9bb25148f90750ff92ac655c9c1b194a91a43fae
Xavierxxxa <xavier.g13@outlook.com>
2021-02-21 15:26:06 -0300

    Atualizando arquivo de conexão

--- Index/config/database.js
+++ Index/config/database.js
@@ -1,9 +1,9 @@
 const mysql = require('mysql');
 module.exports = function(){
     return mysql.createConnection({ //Criando conexão com banco
-        host: "mysql-20992-0.cloudclusters.net",
-        user: "squad5",
-        password: "squad5@@",
-        database: "squad5"
+        host: "us-cdbr-east-03.cleardb.com",
+        user: "b6981eeb6b1980",
+        password: "a495a6fd",
+        database: "heroku_6cf099ec35994cc"
     });
 }
