ea449b4d866779f594654bce5042ea333ddda862
Xavierxxxa <xavier.g13@outlook.com>
2021-02-21 18:05:53 -0300

    Atualizações  finais

--- index.js
+++ index.js
@@ -18,7 +18,12 @@ const database = connection();
 
 
 //Middlewares
-    server.use(cors()); //Permite a leitura de fontes externas
+server.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", 'GET,PUT,POST');
+        server.use(cors());
+        next();
+});
 
     server.use(cookie())
 
