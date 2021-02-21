21ff22b585572fc98dea1060555face454900ffb
Xavierxxxa <xavier.g13@outlook.com>
2021-02-21 16:11:34 -0300

    testando  audit

--- index.js
+++ index.js
@@ -250,7 +250,7 @@ server.post('/audit/retorna', function(req,res){
 	const email = req.body.email;
 	console.log(email)
 
-    const sql = `SELECT audit FROM login WHERE email='${email}'`;
+    const sql = `SELECT audit FROM login WHERE email='xavier.g113@outlook.com'`;
     database.query(sql, (error, resultado) =>{
         if(error){
             console.log(error)
