9561613f8ba593bbfb1850e33da555ee1a0d6da1
Xavierxxxa <xavier.g13@outlook.com>
2021-02-21 23:04:42 -0300

    Atualizações  finais

--- index.js
+++ index.js
@@ -286,4 +286,5 @@ server.post('/audit/retorna', function(req,res){
 const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`)
+})
