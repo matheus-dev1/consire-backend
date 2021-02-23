24c5b6000a43dfe9e329f5837b818df1778bc59e
Xavierxxxa <xavier.g13@outlook.com>
2021-02-23 12:11:18 -0300

    Atualizando arquivo de conexão

--- Index/config/database.js
+++ Index/config/database.js
@@ -1,9 +1,11 @@
 const mysql = require('mysql');
 module.exports = function(){
-    return mysql.createConnection({ //Criando conexão com banco
+    return mysql.create({ //Criando conexão com banco
         host: "us-cdbr-east-03.cleardb.com",
         user: "b6981eeb6b1980",
         password: "a495a6fd",
         database: "heroku_6cf099ec35994cc"
     });
 }
+
+mysql.createPO
