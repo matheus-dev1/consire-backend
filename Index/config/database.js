a04329ca15f8b851adb6713eb06d597eca87660d
Xavierxxxa <xavier.g13@outlook.com>
2021-02-23 12:16:23 -0300

    Atualizando database

--- Index/config/database.js
+++ Index/config/database.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 module.exports = function(){
-    return mysql.create({ //Criando conexão com banco
+    return mysql.createPool({ //Criando conexão com banco
         host: "us-cdbr-east-03.cleardb.com",
         user: "b6981eeb6b1980",
         password: "a495a6fd",
@@ -8,4 +8,3 @@ module.exports = function(){
     });
 }
 
-mysql.createPO
